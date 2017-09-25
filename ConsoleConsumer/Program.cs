ad05262a4a61419df29f679a92244d4d510b2ff3
Rasmus Lunddahl Overbye <borgzilla@gmail.com>
2017-09-25 10:46:12 +0200

    Add project files.

---
+++ ConsoleConsumer/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleConsumer.Calculator;
+
+namespace ConsoleConsumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (Service1Client client = new Service1Client())
+            {
+                int adding = client.Add(5, 5);
+                Console.WriteLine(adding);
+            }
+            using (Service1Client client = new Service1Client())
+            {
+                int substract = client.Sub(5, 5);
+                Console.WriteLine(substract);
+            }
+            using (Service1Client client = new Service1Client())
+            {
+                int divid = client.Div(5, 5);
+                Console.WriteLine(divid);
+            }
+            using (Service1Client client = new Service1Client())
+            {
+                int multi = client.Mul(5, 5);
+                Console.WriteLine(multi);
+            }
+
+        }
+    }
+}
---
+++ UnitTestCalculator/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestCalculator")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestCalculator")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("56f031c9-66e0-4402-8912-cede3296ef04")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
